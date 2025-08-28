260e5f4395d5b93f9a116ff264330435cb0038b0
Aakash Pandey <aakash@aiyensi.com>
2025-08-28 18:31:29 +0530

    delete the file

--- tests/api/api.chat.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect, request } from '@playwright/test';
-
-test('basic API test', async () => {
-  const apiContext = await request.newContext();
-  const response = await apiContext.get('http://localhost:3000/api/user');
-  expect(response.ok()).toBeTruthy();
-  const data = await response.json();
-  expect(data).toEqual({ id: 1, name: 'Alice' });
-});
