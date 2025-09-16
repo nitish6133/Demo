9d716d058ba67dc2867775c2dd3279316e08d83f
Aakash Pandey <aakash@aiyensi.com>
2025-09-16 10:56:58 +0530

    delete stripe service

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// Main package exports
-export { default as StripePaymentForm } from './components/StripePaymentForm';
-export { stripeService } from './services/stripeService';
-export type { 
-  StripePaymentFormProps, 
-  PaymentFormData, 
-  PaymentIntentResponse,
-  VerificationResponse 
-} from './types/stripeTypes';
